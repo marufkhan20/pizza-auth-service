99e45f2d54822abf56fbabed12a3044e96e3b66e
developmentd420 <development.d420@gmail.com>
2024-01-16 16:19:38 +0600

    add test for persist user

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,8 +1,16 @@
 import express from "express";
+import { AppDataSource } from "../config/data-source";
 import { AuthController } from "../controllers/AuthController";
+import { User } from "../entity/User";
+import { UserService } from "../services/UserService";
 
 const router = express.Router();
-const authController = new AuthController();
+
+// get user repository
+const userRepository = AppDataSource.getRepository(User);
+
+const userService = new UserService(userRepository);
+const authController = new AuthController(userService);
 
 router.post("/register", (req, res) => authController.register(req, res));
 
--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -1,7 +1,26 @@
 import request from "supertest";
+import { DataSource } from "typeorm";
 import app from "../../src/app";
+import { AppDataSource } from "../../src/config/data-source";
+import { User } from "../../src/entity/User";
+import { truncateTables } from "../utils";
 
 describe("POST /auth/register", () => {
+  let connection: DataSource;
+
+  beforeAll(async () => {
+    connection = await AppDataSource.initialize();
+  });
+
+  beforeEach(async () => {
+    // database truncate
+    await truncateTables(connection);
+  });
+
+  afterAll(async () => {
+    await connection.destroy();
+  });
+
   describe("Given all fields", () => {
     it("should return the 201 status code", async () => {
       // Arrange
@@ -47,10 +66,15 @@ describe("POST /auth/register", () => {
       };
 
       // Act
-      const response = await request(app).post("/auth/register").send(userData);
+      await request(app).post("/auth/register").send(userData);
 
       // Assert
-      response;
+      const userRepository = connection.getRepository(User);
+      const users = await userRepository.find();
+      expect(users).toHaveLength(1);
+      expect(users[0].firstName).toBe(userData.firstName);
+      expect(users[0].lastName).toEqual(userData.lastName);
+      expect(users[0].email).toEqual(userData.email);
     });
   });
   describe("Fields are missing", () => {});
