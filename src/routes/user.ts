73d040915e8ddad6585e06f805852433b2e43d16
developmentd420 <development.d420@gmail.com>
2024-03-12 01:15:53 +0600

    Add user delete, get all, get single and update endpoint

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,20 +1,50 @@
-import express from "express";
+import express, { NextFunction, Response } from "express";
 import { AppDataSource } from "../config/data-source";
+import logger from "../config/logger";
 import { Roles } from "../constants";
 import { UserController } from "../controllers/UserController";
 import { User } from "../entity/User";
 import authenticate from "../middlewares/authenticate";
 import { canAccess } from "../middlewares/canAccess";
 import { UserService } from "../services/UserService";
+import { UpdateUserRequest } from "../types";
+import updateUserValidator from "../validators/updateUserValidator";
 const router = express.Router();
 
 const userRepository = AppDataSource.getRepository(User);
 const userService = new UserService(userRepository);
-const userController = new UserController(userService);
+const userController = new UserController(userService, logger);
 
 // create a user
 router.post("/", authenticate, canAccess([Roles.ADMIN]), (req, res, next) =>
   userController.create(req, res, next),
+);
+
+router.patch(
+  "/:id",
+  authenticate,
+  canAccess([Roles.ADMIN]),
+  updateUserValidator,
+  (req: UpdateUserRequest, res: Response, next: NextFunction) =>
+    userController.update(req, res, next),
+);
+
+// get all users
+router.get("/", authenticate, canAccess([Roles.ADMIN]), (req, res, next) =>
+  userController.getAll(req, res, next),
+);
+
+// get single user
+router.get("/:id", authenticate, canAccess([Roles.ADMIN]), (req, res, next) =>
+  userController.getOne(req, res, next),
+);
+
+// delete user
+router.delete(
+  "/:id",
+  authenticate,
+  canAccess([Roles.ADMIN]),
+  (req, res, next) => userController.destroy(req, res, next),
 );
 
 export default router;
--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -2,7 +2,7 @@ import bcrypt from "bcrypt";
 import createHttpError from "http-errors";
 import { Repository } from "typeorm";
 import { User } from "../entity/User";
-import { UserData } from "../types";
+import { LimitedUserData, UserData } from "../types";
 
 export class UserService {
   constructor(private userRepository: Repository<User>) {}
@@ -48,5 +48,29 @@ export class UserService {
     const user = await this.userRepository.findOne({ where: { id } });
 
     return user;
+  }
+
+  async update(userId: number, { firstName, lastName, role }: LimitedUserData) {
+    try {
+      return await this.userRepository.update(userId, {
+        firstName,
+        lastName,
+        role,
+      });
+    } catch (err) {
+      const error = createHttpError(
+        500,
+        "Failed to update the user in the database",
+      );
+      throw error;
+    }
+  }
+
+  async getAll() {
+    return await this.userRepository.find();
+  }
+
+  async deleteById(userId: number) {
+    return await this.userRepository.delete(userId);
   }
 }
