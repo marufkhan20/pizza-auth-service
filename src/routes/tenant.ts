6382791c2e073d4a49d1c50872dca1ba9323d515
developmentd420 <development.d420@gmail.com>
2024-03-11 10:57:39 +0600

    Add create tenant endpoint

--- src/routes/tenant.ts
+++ src/routes/tenant.ts
@@ -1,8 +1,15 @@
 import express from "express";
+import { AppDataSource } from "../config/data-source";
+import logger from "../config/logger";
+import { TenantController } from "../controllers/TenantController";
+import { Tenant } from "../entity/Tenant";
+import { TennatService } from "../services/TenantService";
 const router = express.Router();
 
-router.post("/", (req, res) => {
-  res.status(201).json({});
-});
+const tenantRepository = AppDataSource.getRepository(Tenant);
+const tenantService = new TennatService(tenantRepository);
+const tenantController = new TenantController(tenantService, logger);
+
+router.post("/", (req, res, next) => tenantController.create(req, res, next));
 
 export default router;
--- tests/tenants/create.spec.ts
+++ tests/tenants/create.spec.ts
@@ -2,6 +2,7 @@ import request from "supertest";
 import { DataSource } from "typeorm";
 import app from "../../src/app";
 import { AppDataSource } from "../../src/config/data-source";
+import { Tenant } from "../../src/entity/Tenant";
 
 describe("POST /tenants", () => {
   let connection: DataSource;
@@ -32,6 +33,25 @@ describe("POST /tenants", () => {
 
       // Assert
       expect(response.statusCode).toBe(201);
+    });
+
+    it("Should create a tenant in the database", async () => {
+      // Arrange
+      const tenantData = {
+        name: "Tenant name",
+        address: "Tenant address",
+      };
+
+      // Act
+      await request(app).post("/tenants").send(tenantData);
+
+      const tenantRepository = connection.getRepository(Tenant);
+      const tenants = await tenantRepository.find();
+
+      // Assert
+      expect(tenants).toHaveLength(1);
+      expect(tenants[0].name).toBe(tenantData.name);
+      expect(tenants[0].address).toBe(tenantData.address);
     });
   });
 });
