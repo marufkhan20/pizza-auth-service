0ff150861635672f0b339d9dd1652f85f4235f94
developmentd420 <development.d420@gmail.com>
2024-03-25 16:22:40 +0600

    replace bcrypt with bcryptjs

--- tests/users/login.spec.ts
+++ tests/users/login.spec.ts
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import request from "supertest";
 import { DataSource } from "typeorm";
 import app from "../../src/app";
@@ -51,8 +51,8 @@ describe("POST /auth/login", () => {
         ["set-cookie"]: string[];
       }
       // Assert
-      let accessToken = null;
-      let refreshToken = null;
+      let accessToken: string | null = null;
+      let refreshToken: string | null = null;
       const cookies =
         (response.headers as unknown as Headers)["set-cookie"] || [];
       cookies.forEach((cookie) => {
