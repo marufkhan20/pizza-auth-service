f4a429d098f8edee68d0e53d118c1817df94c60a
Rashedul Islam Maruf <mdm326040@gmail.com>
2025-03-28 01:58:47 +0600

    Add express server

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,16 @@
+import app from "./app";
 import { Config } from "./config";
 
-console.log(Config.PORT);
+const startServer = () => {
+  const PORT = Config.PORT;
+  try {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+startServer();
